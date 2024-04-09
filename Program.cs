8689a59a5b500a93007591766481d57c0c826559
Aditya Jindal <165794659+adityajvt@users.noreply.github.com>
2024-04-09 17:59:44 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace attackGame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Game game = new Game();
+        game.StartGame();
+    }
+}
